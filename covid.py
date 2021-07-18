225be3a64c2c3e97e2ccce6deb43233aa1e16481
Fernando Sousa <fernando.sousa@faculdadeimpacta.com.br>
2021-07-18 13:57:42 +0000

    porta padrão

--- covid.py
+++ covid.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from waitress import serve
 
 app = Flask('__name__')
 
@@ -8,4 +7,4 @@ def home():
     return "OK"
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=80)
+    app.run(host="0.0.0.0",port=5000)
